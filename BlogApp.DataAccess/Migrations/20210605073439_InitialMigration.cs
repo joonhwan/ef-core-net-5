5a196ab32fadaf0a7bccb51273e1bef933dc31dd
Joonhwan <joonhwan.lee@gmail.com>
2021-06-05 22:39:20 +0900

    4. 두번째 Migration 생성하기

---
+++ BlogApp.DataAccess/Migrations/20210605073439_InitialMigration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogApp.DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true),
+                    Url = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "1, 1"),
+                    BlogId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Title = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true),
+                    Content = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
---
+++ BlogApp.DataAccess/Migrations/20210605132934_AddBlogPostRelation.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogApp.DataAccess.Migrations
+{
+    public partial class AddBlogPostRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BlogId",
+                table: "Posts",
+                type: "NUMBER(10)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Blogs_BlogId",
+                table: "Posts",
+                column: "BlogId",
+                principalTable: "Blogs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Blogs_BlogId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BlogId",
+                table: "Posts",
+                type: "NUMBER(10)",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)",
+                oldNullable: true);
+        }
+    }
+}
