2f6a61c479c97b4c6d18809f6d99d960ffae0a5e
Joonhwan <joonhwan.lee@gmail.com>
2021-06-05 15:17:08 +0900

    2. 필요한 nuget package 추가하기

---
+++ BlogApp.DataAccess/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using BlogApp.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.DataAccess
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var connectionString = "User Id=BLOGDB;Password=BLOGUSER;Data Source=localhost:1521/XE";
+            optionsBuilder.UseOracle(connectionString, builder =>
+            {
+                builder.UseOracleSQLCompatibility("11");
+            });
+        }
+    }
+}
